99895cde07a30f82ed9db283908164d58ae99ec1
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-04-07 20:09:30 +0100

    created test file, get, put, del for a video endpoint

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+data = [{"name": "Hag", "views": 20000, "likes": 3000}, {"name": "Wraith", "views": 8000, "likes": 9000}, {"name": "Trapper", "views": 90000, "likes": 100000}, {"name": "Legion", "views": 60000, "likes": 78964}]
+
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+
+deleted = requests.delete(BASE + "video/0")
+print (deleted)
