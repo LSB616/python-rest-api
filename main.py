efa35b83ae048dc35216cb8dbdfcd7971878c854
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-04-08 23:00:43 +0100

    added patch functionality

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ video_put_args.add_argument("name", type=str, help="Name of the video required",
 video_put_args.add_argument("views", type=int, help="Views of the video required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video required")
+video_update_args.add_argument("views", type=int, help="Views of the video required")
+video_update_args.add_argument("likes", type=int, help="Likes on the video required")
+
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -32,6 +38,8 @@ class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
         return result
 
     @marshal_with(resource_fields)
@@ -39,12 +47,29 @@ class Video(Resource):
         args = video_put_args.parse_args()
         result = VideoModel.query.filter_by(id=video_id).first()
         if result:
-            abort(409, message="Video id taken...")
+            abort(409, message="Video ID taken...")
         video = VideoModel(id = video_id, name = args['name'], views = args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
         return video, 201
     
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+        
+        db.session.commit()
+
+        return result
+
     def delete(self, video_id):
         # del videos[video_id]
         return '', 204
--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"name": "Hag", "views": 20000, "likes": 3000}, {"name": "Wraith", "views": 8000, "likes": 9000}, {"name": "Trapper", "views": 90000, "likes": 100000}, {"name": "Legion", "views": 60000, "likes": 78964}]
+# data = [{"name": "Hag", "views": 20000, "likes": 3000}, {"name": "Wraith", "views": 8000, "likes": 9000}, {"name": "Trapper", "views": 90000, "likes": 100000}, {"name": "Legion", "views": 60000, "likes": 78964}]
 
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
+
+# response = requests.get(BASE + "video/8")
+# print(response.json())
+
+patch_response = requests.patch(BASE + "video/1", {"views": 2347, "likes": "3457"})
+print(patch_response.json())
